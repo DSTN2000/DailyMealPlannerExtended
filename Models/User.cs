0d125d368dc17d46943bf4b305fddc4b4efd4926
DSTN2000 <dansoltan05@gmail.com>
2025-11-21 19:10:48 +0300

    Fixed the user class

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,29 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Lab4.Models;
 
-public class User
+public partial class User : ObservableObject
 {
     // Input values
-    public double Weight { get; set; } // in kg
-    public double Height { get; set; } // in cm
-    public double Age { get; set; } // in years
-    public ActivityLevel ActivityLevel { get; set; }
-    public (double p, double f, double c) NutrientsSplit {get; set;} // in %
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(BMI), nameof(BMR), nameof(DailyCalories), nameof(DailyProtein), nameof(DailyFat), nameof(DailyCarbohydrates))]
+    private double _weight = 75; // in kg
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(BMI), nameof(BMR), nameof(DailyCalories), nameof(DailyProtein), nameof(DailyFat), nameof(DailyCarbohydrates))]
+    private double _height = 170; // in cm
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(BMR), nameof(DailyCalories), nameof(DailyProtein), nameof(DailyFat), nameof(DailyCarbohydrates))]
+    private double _age = 30; // in years
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ARM), nameof(DailyCalories), nameof(DailyProtein), nameof(DailyFat), nameof(DailyCarbohydrates))]
+    private ActivityLevel _activityLevel = ActivityLevel.Moderate;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(DailyProtein), nameof(DailyFat), nameof(DailyCarbohydrates))]
+    private (double p, double f, double c) _nutrientsSplit = (30, 30, 40); // in %
 
     // Calculated values
     public double BMI => Weight / Math.Pow(Height / 100.0, 2);
@@ -22,16 +38,8 @@ public class User
     public double BMR => 447.593 + 9.247 * Weight + 3.098 * Height - 4.330 * Age;
     public double DailyCalories => BMR * ARM;
     public double DailyProtein => DailyCalories * (NutrientsSplit.p / 100) / 4; // in grams, 4 cal/g
-    public double DailyFat => DailyCalories * (NutrientsSplit.p / 100) / 9; // in grams, 9 cal/g
-    public double DailyCarbohydrates => DailyCalories * (NutrientsSplit.p / 100) / 4; // in grams, 4 cal/g
-
-    public User()
-    {
-        Weight = 75;
-        Height = 170;
-        Age = 30;
-        ActivityLevel = ActivityLevel.Moderate;
-    }
+    public double DailyFat => DailyCalories * (NutrientsSplit.f / 100) / 9; // in grams, 9 cal/g
+    public double DailyCarbohydrates => DailyCalories * (NutrientsSplit.c / 100) / 4; // in grams, 4 cal/g
 }
 
 public enum ActivityLevel
